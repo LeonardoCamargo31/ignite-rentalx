7e40d0064876e4985d8106a0fc7dedd3f45b2c4a
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2021-11-03 12:30:11 -0300

    docs: create documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { routes } from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 
 const app = express()
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
 
 app.listen(3333, () => console.log('Server is running'))
